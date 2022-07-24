b75c59f0ec225c470d942488ace6656cabd906c7
Shishira S Jois <54272489+shishira7@users.noreply.github.com>
2022-07-24 19:45:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,9 +19,9 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 
 def clean(x):
-    x = re.sub(r'[^a-zA-Z ]', ' ', x) # replace evrything thats not an alphabet with a space
-    x = re.sub(r'\s+', ' ', x) #replace multiple spaces with one space
-    x = re.sub(r'READ MORE', '', x) # remove READ MORE
+    x = re.sub(r'[^a-zA-Z ]', ' ', x)
+    x = re.sub(r'\s+', ' ', x)
+    x = re.sub(r'READ MORE', '', x)
     x = x.lower()
     x = x.split()
     y = []
@@ -71,25 +71,9 @@ def extract_all_reviews(url, clean_reviews, org_reviews,customernames,commenthea
             ra.append(r.get_text())
         
     ratings += ra
-    print(ratings)
+    #print(ratings)
 
-def tokenizer(s):
-    s = s.lower()      # convert the string to lower case
-    tokens = nltk.tokenize.word_tokenize(s) # make tokens ['dogs', 'the', 'plural', 'for', 'dog']
-    tokens = [t for t in tokens if len(t) > 2] # remove words having length less than 2
-    tokens = [t for t in tokens if t not in stop_words] # remove stop words like is,and,this,that etc.
-    return tokens
 
-def tokens_2_vectors(token):
-    X = np.zeros(len(word_2_int)+1)
-    for t in token:
-        if t in word_2_int:
-            index = word_2_int[t]
-        else:
-            index = 0
-        X[index] += 1
-    X = X/X.sum()
-    return X
 
 
 @app.route('/')
@@ -114,17 +98,17 @@ def result():
     proname = page_html.find_all('span', {'class': 'B_NuCI'})[0].get_text()
     price = page_html.find_all('div', {'class': '_30jeq3 _16Jk6d'})[0].get_text()
     
-    # getting the link of see all reviews button
+    
     all_reviews_url = page_html.find_all('div', {'class': 'col JOpGWq'})[0]
     all_reviews_url = all_reviews_url.find_all('a')[-1]
     all_reviews_url = 'https://www.flipkart.com'+all_reviews_url.get('href')
     url2 = all_reviews_url+'&page=1'
     
 
-    # start reading reviews and go to next page after all reviews are read 
+   
     while True:
         x = len(clean_reviews)
-        # extracting the reviews
+        
         extract_all_reviews(url2, clean_reviews, org_reviews,customernames,commentheads,ratings)
         url2 = url2[:-1]+str(int(url2[-1])+1)
         if x == len(clean_reviews) or len(clean_reviews)>=nreviews:break
@@ -134,9 +118,14 @@ def result():
     customernames = customernames[:nreviews]
     commentheads = commentheads[:nreviews]
     ratings = ratings[:nreviews]
+    
+    
+    #print(clean_reviews)
+    #print(org_reviews)
+    #print(customernames)
+    #print(commentheads)
 
-
-    # building our wordcloud and saving it
+    
     for_wc = ' '.join(clean_reviews)
     wcstops = set(STOPWORDS)
     wc = WordCloud(width=1400,height=800,stopwords=wcstops,background_color='white').generate(for_wc)
@@ -149,12 +138,11 @@ def result():
     plt.close()
 
     
-    # making a dictionary of product attributes and saving all the products in a list
+    
     d = []
     for i in range(len(org_reviews)):
         x = {}
         x['review'] = org_reviews[i]
-        # x['sent'] = predictions[i]
         x['cn'] = customernames[i]
         x['ch'] = commentheads[i]
         x['stars'] = ratings[i]
@@ -183,15 +171,10 @@ def wc():
 
 
 class CleanCache:
-	'''
-	this class is responsible to clear any residual csv and image files
-	present due to the past searches made.
-	'''
+	
 	def __init__(self, directory=None):
 		self.clean_path = directory
-		# only proceed if directory is not empty
 		if os.listdir(self.clean_path) != list():
-			# iterate over the files and remove each file
 			files = os.listdir(self.clean_path)
 			for fileName in files:
 				print(fileName)
